85292893c5578ea41a3cf883779d8c4c7a7409c3
nandikajain <jainnandika@gmail.com>
2021-03-16 01:43:55 +0530

    ScrollToTop implemented

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import History from "./pages/History/history"
 import Reimbursements from "./pages/History/Reimbursements"
 import Request from "./pages/Request/Request"
 import { HashRouter, Route, Switch} from 'react-router-dom'
+import ScrollToTop from "./ScrollToTop"
 
 function App() {
   return (
     <>
       <HashRouter basename="/">
+        <ScrollToTop/>
         <Switch>
           <Route exact path="/" >
               <Home />
